0fffd8b96c4e039a857e0045de25aae84ebc79e6
yungblud <killi8n@gmail.com>
2024-03-09 14:55:41 +0900

    feat: implemented routes.rs

---
+++ src/routes.rs
@@ -0,0 +1,11 @@
+use warp::Filter;
+use super::handlers;
+
+pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    get_post()
+}
+fn get_post() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("posts" / u64)
+        .and(warp::get())
+        .and_then(handlers::get_post)
+}
